e23c07e76e98e6f79de9235030f297def10d3b5b
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-02-25 21:13:50 -0500

    created class for basic operations

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/operation.java
@@ -0,0 +1,21 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class operation {
+
+    public static double addition( double a, double b){
+        double sum = a + b;
+        return sum;
+    }
+    public static double substraction( double a, double b) {
+        double subtract = a - b;
+        return subtract;
+    }
+    public static double multiplication( double a, double b) {
+        double multiply = a * b;
+        return multiply;
+    }
+    public static double divison( double a, double b) {
+        double divide = a / b;
+        return divide;
+    }
+}

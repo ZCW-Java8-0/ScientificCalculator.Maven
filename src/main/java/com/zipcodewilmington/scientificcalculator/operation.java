1c20cf17939b51ad5b61d57d1380e2f7f47ea53c
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-02-25 21:25:12 -0500

    square operations added

--- src/main/java/com/zipcodewilmington/scientificcalculator/operation.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/operation.java
@@ -2,24 +2,37 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class operation {
 
-    public static double addition( double a, double b){
+    public static double addition(double a, double b) {
         double sum = a + b;
         return sum;
     }
-    public static double substraction( double a, double b) {
+
+    public static double substraction(double a, double b) {
         double subtract = a - b;
         return subtract;
     }
-    public static double multiplication( double a, double b) {
+
+    public static double multiplication(double a, double b) {
         double multiply = a * b;
         return multiply;
     }
-    public static double divison( double a, double b) {
+
+    public static double divison(double a, double b) {
         double divide = 0;
         if (b == 0) System.out.println("Err");
         else {
             divide = a / b;
         }
         return divide;
+
+    }
+
+    public static double square(double a) {
+        double squared = a * a;
+        return squared;
+    }
+    public static double squareRoot(double a) {
+        double squareR = Math.sqrt(a);
+        return squareR;
     }
 }
